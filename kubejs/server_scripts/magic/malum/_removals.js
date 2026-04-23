eeb275f2da5035f86c0162c42db4adf1b3b0a779
Jyhrie <104437222+Jyhrie@users.noreply.github.com>
2026-04-24 01:42:53 +0800

    Added Spirit Recipes

--- kubejs/server_scripts/magic/malum/_removals.js
+++ kubejs/server_scripts/magic/malum/_removals.js
@@ -4,5 +4,6 @@ ServerEvents.recipes((event) => {
   event.remove({ output: "malum:runewood_sapling" });
   event.remove({ output: "malum:azure_runewood_sapling" });
   event.remove({ output: "malum:hallowed_gold_ingot" });
+  event.remove({ id: "malum:spirit_infusion/hallowed_gold_ingot" });
   event.remove({ type: 'malum:spirit_focusing' })
 });
--- kubejs/server_scripts/magic/malum/spirits.js
+++ kubejs/server_scripts/magic/malum/spirits.js
@@ -1,2 +1,315 @@
-ServerEvents.recipes(event => {
-})
+ServerEvents.recipes((event) => {
+  event.custom({
+    type: "lychee:item_inside",
+    item_in: {
+      item: "malum:raw_soulstone",
+    },
+    block_in: {
+      blocks: ["starbunclemania:source_fluid_block"],
+      state: {
+        level: 0, // Ensures it only works in a full source block
+      },
+    },
+    post: [
+      {
+        type: "drop_item",
+        item: "malum:wicked_spirit",
+      },
+      {
+        type: "drop_item",
+        item: "malum:wicked_spirit",
+        contextual: [
+          {
+            type: "chance",
+            chance: 0.5,
+          },
+        ],
+      },
+      {
+        type: "drop_item",
+        item: "malum:wicked_spirit",
+        contextual: [
+          {
+            type: "chance",
+            chance: 0.3,
+          },
+        ],
+      },
+      {
+        type: "drop_item",
+        item: "malum:infernal_spirit",
+      },
+      {
+        type: "drop_item",
+        item: "malum:infernal_spirit",
+        contextual: [
+          {
+            type: "chance",
+            chance: 0.5,
+          },
+        ],
+      },
+      {
+        type: "drop_item",
+        item: "malum:infernal_spirit",
+        contextual: [
+          {
+            type: "chance",
+            chance: 0.3,
+          },
+        ],
+      },
+      {
+        type: "drop_item",
+        item: "malum:eldritch_spirit",
+      },
+      {
+        type: "drop_item",
+        item: "malum:eldritch_spirit",
+        contextual: [
+          {
+            type: "chance",
+            chance: 0.5,
+          },
+        ],
+      },
+      {
+        type: "drop_item",
+        item: "malum:eldritch_spirit",
+        contextual: [
+          {
+            type: "chance",
+            chance: 0.3,
+          },
+        ],
+      },
+      {
+        type: "place",
+        block: "minecraft:air",
+        contextual: [
+          {
+            type: "chance",
+            chance: 0.1,
+          },
+        ],
+      },
+      {
+        type: "execute",
+        command:
+          "playsound minecraft:block.amethyst_block.chime ambient @a ~ ~ ~ 1 1.2",
+      },
+      {
+        type: "execute",
+        command: "particle minecraft:enchant ~ ~0.5 ~ 0.5 0.5 0.5 0.1 100",
+      },
+    ],
+  });
+
+  event.custom({
+    type: "lychee:item_inside",
+    item_in: {
+      item: "malum:cluster_of_brilliance",
+    },
+    block_in: {
+      blocks: ["starbunclemania:source_fluid_block"],
+      state: {
+        level: 0, // Ensures it only works in a full source block
+      },
+    },
+    post: [
+      {
+        type: "drop_item",
+        item: "malum:aerial_spirit",
+      },
+      {
+        type: "drop_item",
+        item: "malum:aerial_spirit",
+        contextual: [
+          {
+            type: "chance",
+            chance: 0.5,
+          },
+        ],
+      },
+      {
+        type: "drop_item",
+        item: "malum:aerial_spirit",
+        contextual: [
+          {
+            type: "chance",
+            chance: 0.3,
+          },
+        ],
+      },
+      {
+        type: "drop_item",
+        item: "malum:aqueous_spirit",
+      },
+      {
+        type: "drop_item",
+        item: "malum:aqueous_spirit",
+        contextual: [
+          {
+            type: "chance",
+            chance: 0.5,
+          },
+        ],
+      },
+      {
+        type: "drop_item",
+        item: "malum:aqueous_spirit",
+        contextual: [
+          {
+            type: "chance",
+            chance: 0.3,
+          },
+        ],
+      },
+      {
+        type: "drop_item",
+        item: "malum:earthen_spirit",
+      },
+      {
+        type: "drop_item",
+        item: "malum:earthen_spirit",
+        contextual: [
+          {
+            type: "chance",
+            chance: 0.5,
+          },
+        ],
+      },
+      {
+        type: "drop_item",
+        item: "malum:earthen_spirit",
+        contextual: [
+          {
+            type: "chance",
+            chance: 0.3,
+          },
+        ],
+      },
+      {
+        type: "place",
+        block: "minecraft:air",
+        contextual: [
+          {
+            type: "chance",
+            chance: 0.1,
+          },
+        ],
+      },
+      {
+        type: "execute",
+        command:
+          "playsound minecraft:block.amethyst_block.chime ambient @a ~ ~ ~ 1 1.2",
+      },
+      {
+        type: "execute",
+        command: "particle minecraft:enchant ~ ~0.5 ~ 0.5 0.5 0.5 0.1 100",
+      },
+    ],
+  });
+
+  event.custom({
+    type: "lychee:item_inside",
+    item_in: {
+      item: "kubejs:raw_arcane",
+    },
+    block_in: {
+      blocks: ["starbunclemania:source_fluid_block"],
+      state: {
+        level: 0, // Ensures it only works in a full source block
+      },
+    },
+    post: [
+      {
+        type: "drop_item",
+        item: "malum:sacred_spirit",
+      },
+      {
+        type: "drop_item",
+        item: "malum:sacred_spirit",
+        contextual: [
+          {
+            type: "chance",
+            chance: 0.5,
+          },
+        ],
+      },
+      {
+        type: "drop_item",
+        item: "malum:sacred_spirit",
+        contextual: [
+          {
+            type: "chance",
+            chance: 0.3,
+          },
+        ],
+      },
+    {
+        type: "drop_item",
+        item: "malum:sacred_spirit",
+        contextual: [
+          {
+            type: "chance",
+            chance: 0.3,
+          },
+        ],
+      },
+      {
+        type: "drop_item",
+        item: "malum:arcane_spirit",
+      },
+      {
+        type: "drop_item",
+        item: "malum:arcane_spirit",
+        contextual: [
+          {
+            type: "chance",
+            chance: 0.5,
+          },
+        ],
+      },
+      {
+        type: "drop_item",
+        item: "malum:arcane_spirit",
+        contextual: [
+          {
+            type: "chance",
+            chance: 0.3,
+          },
+        ],
+      },
+            {
+        type: "drop_item",
+        item: "malum:arcane_spirit",
+        contextual: [
+          {
+            type: "chance",
+            chance: 0.3,
+          },
+        ],
+      },
+      {
+        type: "place",
+        block: "minecraft:air",
+        contextual: [
+          {
+            type: "chance",
+            chance: 0.1,
+          },
+        ],
+      },
+      {
+        type: "execute",
+        command:
+          "playsound minecraft:block.amethyst_block.chime ambient @a ~ ~ ~ 1 1.2",
+      },
+      {
+        type: "execute",
+        command: "particle minecraft:enchant ~ ~0.5 ~ 0.5 0.5 0.5 0.1 100",
+      },
+    ],
+  });
+});
