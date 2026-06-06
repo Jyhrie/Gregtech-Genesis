6b1a77f85803caad7eb103032dcbf4bd83ff7bc6
Jyhrie <104437222+Jyhrie@users.noreply.github.com>
2026-06-07 01:36:37 +0800

    Ancient Elements Features Removed

---
+++ kubejs/startup_scripts/worldgen/remove_features_ancient_elements.js
@@ -0,0 +1,43 @@
+WorldgenEvents.remove(event => {
+    event.removeFeatureById("underground_ores", [
+        "ancient_elements:abyssal_debris",
+        "ancient_elements:aetherium_ore",
+        "ancient_elements:ancient_metal_debris",
+        "ancient_elements:celestium_ore",
+        "ancient_elements:deepslate_lead_ore",
+        "ancient_elements:deepslate_jungle_steel_ore",
+        "ancient_elements:deepslate_tin_ore",
+        "ancient_elements:deepslate_titanium_ore",
+        "ancient_elements:ender_steel_ore",
+        "ancient_elements:endrium_ore",
+        "ancient_elements:frost_ore",
+        "ancient_elements:galatic_debris",
+        "ancient_elements:infernal_ore",
+        "ancient_elements:jungle_steel_ore",
+        "ancient_elements:lead_ore",
+        "ancient_elements:meteorite_ore",
+        "ancient_elements:nether_steel_ore",
+        "ancient_elements:palladium_ore",
+        "ancient_elements:spectrillium_ore",
+        "ancient_elements:tin_ore",
+        "ancient_elements:titanium_ore",
+        "ancient_elements:ore_ancient_debris_large",
+        "ancient_elements:ore_ancient_debris_small",
+    ])
+
+    // event.removeFeatureById("underground_decoration", [
+    //     "malum:deepslate_quartz_geode",
+    //     "malun:quartz_geode"
+    // ])
+
+    // event.removeFeatureById("underground_structures", [
+    //     "malum:weeping_well",
+    // ])
+
+    // event.removeFeatureById("vegetal_decoration", [
+    //     "malum:runewood_tree",
+    //     "malum:azure_runewood_tree",
+    //     "malum:rare_runewood_tree",
+    //     "malum:rare_azure_runewood_tree",
+    // ])
+})
